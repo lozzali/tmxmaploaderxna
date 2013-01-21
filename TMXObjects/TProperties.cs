2c9f2c05eee7830fb3d1d79ce4a6e86a3c652c04
Lawrence Li <lawrenceli@windowsphonedevelopments.com>
2013-01-21 22:41:16 +0000

    Revert "test"
    This reverts commit b42ee61222756512b8308d37efff751186ebe5a5.

--- TMXObjects/TProperties.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TMXObjects
-{
-    //The TProperties class inherits the Dictionary class with the name of the property being the key and the value being the value
-    public class TProperties : Dictionary<string, string>
-    {
-    }
-}
